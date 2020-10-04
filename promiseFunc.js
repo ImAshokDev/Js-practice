bb5c5e062ac6bc1dda7e356afc619efb1fd2ff9e
ashokdev10 <imashokdev@gmail.com>
2020-10-04 14:58:55 +0530

    promise chain

--- promiseFunc.js
+++ promiseFunc.js
@@ -35,19 +35,52 @@
 //     console.log("Rejected.....", e);
 //   });
 
-var promise = new Promise(function (resolve, reject) {
-  var num = Math.random();
-  if (num < 0.5) {
-    resolve(num);
-  } else {
-    reject(num);
-  }
-});
+// var promise = new Promise(function (resolve, reject) {
+//   var num = Math.random();
+//   if (num < 0.5) {
+//     resolve(num);
+//   } else {
+//     reject(num);
+//   }
+// });
 
+// promise
+//   .then(function (e) {
+//     console.log("promise resolved....", e);
+//   })
+//   .catch((e) => {
+//     console.log("Promise Rejected.....", e);
+//   });
+
+// promise with async code
+// var promise = new Promise(function (resolve, reject) {
+//   setTimeout(() => {
+//     var num = Math.floor(Math.random() + 10);
+//     resolve(num);
+//   }, 3000);
+// });
+
+// promise
+//   .then(function (e) {
+//     console.log("promise resolved....", e);
+//   })
+//   .catch((e) => {
+//     console.log("Promise Rejected.....", e);
+//   });
+
+// promise chain
+var promise = new Promise(function (resolve, reject) {
+  resolve(5);
+});
 promise
-  .then(function (e) {
-    console.log("promise resolved....", e);
+  .then(function (data) {
+    console.log("result.....1", data);
+    return data * 2;
   })
-  .catch((e) => {
-    console.log("Promise Rejected.....", e);
+  .then(function (data) {
+    console.log("result.....2", data);
+    return data * 2;
+  })
+  .then((data) => {
+    console.log("result.....3", data);
   });
