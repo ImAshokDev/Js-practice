9d08c48d2397026b498dc2f68009e899398d021e
ashokdev10 <imashokdev@gmail.com>
2020-10-04 15:45:22 +0530

    async and await try catch

--- AsyncFunc.js
+++ AsyncFunc.js
@@ -35,11 +35,29 @@
 
 // fetch using async await
 
-async function asyncFunc() {
-  const posts = await fetch("https://jsonplaceholder.typicode.com/posts");
-  const JsonData = await posts.json();
+// async function asyncFunc() {
+//   const posts = await fetch("https://jsonplaceholder.typicode.com/posts");
+//   const JsonData = await posts.json();
 
-  console.log("json post data");
+//   console.log("json post data", JsonData);
+// }
+
+// asyncFunc();
+
+// try catch
+
+async function asyncFunc() {
+  try {
+    const posts = await fetch("https://jsonplaceholder.typicode.com/posts");
+    if (posts.ok === false) {
+      throw "Page not found";
+    }
+    const JsonData = await posts.json();
+    console.log("json post data", JsonData);
+  } catch (err) {
+    console.log(err);
+    console.log("Page 404 Error");
+  }
 }
 
 asyncFunc();
