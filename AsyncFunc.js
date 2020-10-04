6f96e762a903a2818e0d9079e4d5cf52aac28ddb
ashokdev10 <imashokdev@gmail.com>
2020-10-04 15:22:49 +0530

    normal async

--- AsyncFunc.js
+++ AsyncFunc.js
@@ -1,6 +1,12 @@
 // normal Promise Func
-function callMe() {
-  return Promise.resolve("ashok");
+// function callMe() {
+//   return Promise.resolve("ashok");
+// }
+// console.log(callMe());
+
+// normal async
+async function callMe() {
+  return "ashok";
 }
 
 console.log(callMe());
