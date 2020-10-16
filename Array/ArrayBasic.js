53c3e491a5d552aff04dabd587bd8c22f8828105
ashokdev10 <imashokdev@gmail.com>
2020-10-16 07:30:48 +0530

    array concat

---
+++ Array/ArrayBasic.js
@@ -0,0 +1,10 @@
+// array length
+// const a = [1, 2, 3, 4, 5];
+// console.log("arra", a.length);
+
+// array concat
+const a = [1, 2, 3, 4, 5];
+const b = ["a", "b", "c", "d"];
+
+const array3 = a.concat(b);
+console.log("concat....", array3);
