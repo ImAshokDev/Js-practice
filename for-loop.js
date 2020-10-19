cc780313fc2180d2ac9b7a1ae99dcf8ae8b4830b
Ashok <imashokdev@gmail.com>
2020-10-19 13:49:23 +0530

    do while loop

--- for-loop.js
+++ for-loop.js
@@ -2,6 +2,20 @@
 
 const x = 10;
 
-for (i = 1; i <= x; i++) {
-  console.log("i =", i);
+// for loop
+// for (i = 1; i <= x; i++) {
+//   console.log("i =", i);
+// }
+
+// while loop
+var i = 1;
+while (i <= x) {
+  console.log("while i= ", i);
+  i++;
 }
+
+// do while
+do {
+  console.log("while i= ", i);
+  i++;
+} while (i <= x);
