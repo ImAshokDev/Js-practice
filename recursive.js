839aab527b2ce102395cdca17e6793d38bf46a7c
Ashok <imashokdev@gmail.com>
2020-10-19 14:59:31 +0530

    recusive

--- recursive.js
+++ recursive.js
@@ -1,10 +1,21 @@
-function countDown(num) {
-  console.log("before num = ", num);
+// function countDown(num) {
+//   console.log("before num = ", num);
 
-  if (num === 5) {
-    countDown(num - 1);
+//   if (num === 5) {
+//     countDown(num - 1);
+//   }
+//   console.log("after num = ", num);
+// }
+
+// countDown(5);
+
+function countDown(fromNumber) {
+  console.log(fromNumber);
+
+  let nextNumber = fromNumber - 1;
+
+  if (nextNumber > 0) {
+    countDown(nextNumber);
   }
-  console.log("after num = ", num);
 }
-
-countDown(5);
+countDown(3);
