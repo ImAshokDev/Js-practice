b54d775ef04b8525897b48966e4f7a61463ee2a5
Ashok <imashokdev@gmail.com>
2020-10-19 13:43:36 +0530

    for loop added

---
+++ for-loop.js
@@ -0,0 +1,7 @@
+// const a1 = [1, 2, 3, 4, 5];
+
+const x = 10;
+
+for (i = 1; i <= x; i++) {
+  console.log("i =", i);
+}
---
+++ try-catch.js
@@ -0,0 +1,5 @@
+try {
+  alert("hi");
+} catch (e) {
+  console.log("err", e);
+}
