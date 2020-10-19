23f3bd0a3e96feb36a7715f103ea289778701fb6
Ashok <imashokdev@gmail.com>
2020-10-19 13:59:03 +0530

    switch statement added

---
+++ switch.js
@@ -0,0 +1,18 @@
+const a = 2;
+
+switch (a) {
+  case 1:
+    console.log(`The value is = 1`);
+    break;
+  case 2:
+    console.log(`The value is = 2`);
+    break;
+  case 3:
+    console.log(`The value is = 3`);
+    break;
+  case 4:
+    console.log(`The value is = 4`);
+    break;
+  default:
+    console.log(`Default statement `);
+}
