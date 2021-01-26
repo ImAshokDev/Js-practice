6d423713060b43d8f6e0a2e275cc873bb4074a78
Ashokdev10 <imashokdev@gmail.com>
2021-01-26 21:23:53 +0530

    JS String Method

--- src/StringMethod.js
+++ src/StringMethod.js
@@ -1,6 +1,15 @@
-const str = "Ashok Dev";
-const result = str.charAt(2);
+// 1. charAt
+// Returns the character at the specified index.
+// const str = "Ashok Dev";
+// const result = str.charAt(2); //
 // const result = str.charAt(0);
 // const result = str.charAt(20);
+// console.log("Result : ", result);
 
+// 2. charCodeAt
+// Returns the unicode of the character at the specified index.
+const str = "Ashok Dev";
+const result = str.charCodeAt(2);
+// const result = str.charCodeAt();
+// const result = str.charAt(20);
 console.log("Result : ", result);
